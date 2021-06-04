304e3efc1dac273093b9f58dd4a0037242361fde
iasjem <jemimah.beryl.sai@gmail.com>
2021-06-04 15:59:30 +0800

    Add sample GET URL for API

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/amcharts/simple-column-chart', (req, res) => {
+  res.json([{
+    "country": "USA",
+    "visits": 2025
+  }, {
+    "country": "China",
+    "visits": 1882
+  }, {
+    "country": "Japan",
+    "visits": 1809
+  }, {
+    "country": "Germany",
+    "visits": 1322
+  }, {
+    "country": "UK",
+    "visits": 1122
+  }, {
+    "country": "France",
+    "visits": 1114
+  }, {
+    "country": "India",
+    "visits": 984
+  }, {
+    "country": "Spain",
+    "visits": 711
+  }, {
+    "country": "Netherlands",
+    "visits": 665
+  }, {
+    "country": "Russia",
+    "visits": 580
+  }, {
+    "country": "South Korea",
+    "visits": 443
+  }, {
+    "country": "Canada",
+    "visits": 441
+  }, {
+    "country": "Brazil",
+    "visits": 395
+  }]);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
