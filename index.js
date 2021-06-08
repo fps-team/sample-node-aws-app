5780ce4d7710325ca21ef93a6df0a2343327f2ce
iasjem <jemimah.beryl.sai@gmail.com>
2021-06-08 17:30:39 +0800

    Add random unique id to json - /amcharts/simple-column-chart

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8000
 const cors = require("cors")
+const { v4: uuidv4 } = require('uuid');
 
 app.use(cors())
 
@@ -10,46 +11,56 @@ app.get('/', (req, res) => {
 })
 
 app.get('/amcharts/simple-column-chart', (req, res) => {
-  res.json([{
-    "country": "USA",
-    "visits": 2025
-  }, {
-    "country": "China",
-    "visits": 1882
-  }, {
-    "country": "Japan",
-    "visits": 1809
-  }, {
-    "country": "Germany",
-    "visits": 1322
-  }, {
-    "country": "UK",
-    "visits": 1122
-  }, {
-    "country": "France",
-    "visits": 1114
-  }, {
-    "country": "India",
-    "visits": 984
-  }, {
-    "country": "Spain",
-    "visits": 711
-  }, {
-    "country": "Netherlands",
-    "visits": 665
-  }, {
-    "country": "Russia",
-    "visits": 580
-  }, {
-    "country": "South Korea",
-    "visits": 443
-  }, {
-    "country": "Canada",
-    "visits": 441
-  }, {
-    "country": "Brazil",
-    "visits": 395
-  }]);
+  let data = {
+    "properties": {
+      "_id": uuidv4(),
+      "categoryX": "country",
+      "valueY": "visits",
+      "seriesName": "Visits",
+    },
+    "data": [{
+      "country": "USA",
+      "visits": 2025
+    }, {
+      "country": "China",
+      "visits": 1882
+    }, {
+      "country": "Japan",
+      "visits": 1809
+    }, {
+      "country": "Germany",
+      "visits": 1322
+    }, {
+      "country": "UK",
+      "visits": 1122
+    }, {
+      "country": "France",
+      "visits": 1114
+    }, {
+      "country": "India",
+      "visits": 984
+    }, {
+      "country": "Spain",
+      "visits": 711
+    }, {
+      "country": "Netherlands",
+      "visits": 665
+    }, {
+      "country": "Russia",
+      "visits": 580
+    }, {
+      "country": "South Korea",
+      "visits": 443
+    }, {
+      "country": "Canada",
+      "visits": 441
+    }, {
+      "country": "Brazil",
+      "visits": 395
+    }]
+  };
+
+  res.json(data);
 })
 
 app.listen(port, () => {
