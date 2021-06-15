f96448f16eee613b6dac0a755cc69dbc293644d5
iasjem <jemimah.beryl.sai@gmail.com>
2021-06-15 20:41:04 +0800

    Add fixed URL Strings as constants

---
+++ constants.js
@@ -0,0 +1,5 @@
+const BASE_URL = "https://10ax.online.tableau.com";
+const API_VERSION = "/api/3.11";
+const SIGN_IN_URL = BASE_URL + API_VERSION + "/auth/signin";
+
+module.exports = { BASE_URL, SIGN_IN_URL }
