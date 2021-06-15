ce697dae6322ccb493200705d560893521f61809
iasjem <jemimah.beryl.sai@gmail.com>
2021-06-15 21:06:17 +0800

    Add new constant variables and arrange them in a folder

---
+++ constants/auth.js
@@ -0,0 +1,15 @@
+const DEFAULT_CREDENTIALS_EMAIL = "jemimah.beryl.sai@gmail.com";
+const DEFAULT_CREDENTIALS_PASSWORD = "G64*FuB93";
+const DEFAULT_SITE_CONTENT_URI = "samplesite93dev220834";
+
+const TABLEAU_LOGIN_CREDENTIALS = {
+  "credentials": {
+    "name": DEFAULT_CREDENTIALS_EMAIL,
+    "password": DEFAULT_CREDENTIALS_PASSWORD,
+    "site": {
+      "contentUrl": DEFAULT_SITE_CONTENT_URI
+    }
+  }
+};
+
+module.exports = { TABLEAU_LOGIN_CREDENTIALS }
